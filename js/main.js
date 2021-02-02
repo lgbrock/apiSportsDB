52a01382a3edf6a98508dab56f10b2632a539733
Logan Brock <logan.g.brock@gmail.com>
2021-02-02 16:22:14 -0600

    first update

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 //select team - dropdown list or empty textbox
 //get schedule - press button and schedule shows up below
 
-//Example fetch using pokemonapi.co
+
 document.querySelector('#getTeamSchedule').addEventListener('click', getSchedule)
 
 function getSchedule() {
@@ -15,9 +15,8 @@ function getSchedule() {
         .then(res => res.json()) // parse response as JSON
         .then(data => {
             console.log(data)
-            document.querySelector('h1').innerText = data[0].question
-            document.querySelector('h3').innerText = data[0].category.title
-            answer = data[0].answer
+            document.querySelector('h1').innerText = data[0].events.strHomeTeam
+            document.querySelector('h3').innerText = data[0].events
 
         })
         .catch(err => {
@@ -25,36 +24,44 @@ function getSchedule() {
         });
 
 }
-function showAnswer() {
-    document.querySelector('h2').innerText = answer
+function showLeague() {
+    document.querySelector('h2').innerText = showLeague
 }
 
 
 
+/*
+$(function(){
+    fetch("https://www.scorebat.com/video-api/v1/"
+  ).then(res => res.json())
+  .then(element => {
+   let ul = document.querySelector('ul');
+   for(let i = 0; i < element.length; i++){
+     let len = (element[i].videos.length)
+     for(let j=0; j<len; j++){
+     //console.log(element[i].videos)
+      let span = document.createElement('span')
+        span = element[i].date + ". ." + element[i].title
+      let title = element[i].videos[j].title
+      let li = document.createElement('li')
+        li.append(span + "   " + title)
+      let someid = document.getElementById('someId');
+        ul.append(li);
 
+      li.addEventListener('click',function(e){
+        document.documentElement.scrollTop = 0;
+        let div1 =  element[i].videos[j].embed
+        let someid = document.getElementById('someId')
+        someid.innerHTML = div1;
+      })
+       //console.log(title);
+     }
+       }
+  })
+  .catch(err => {
+      console.error(err);
+  });
+  })
 
+  */
 
-
-
-
-
-document.querySelector('button').addEventListener('click', getTeamSchedule)
-
-function getTeamSchedule() {
-    let inputVal = document.querySelector('input').value
-    const url = '' + inputVal
-
-    fetch(url)
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
-            let drinkName = data.drinks[0].strDrink
-            let drinkThumb = data.drinks[0].strDrinkThumb
-            let drinkInst = data.drinks[0].strInstructions
-            document.querySelector('h2').innerText = drinkName
-            document.querySelector('img').src = drinkThumb
-            document.querySelector('h3').innerText = drinkInst
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        })
-}
